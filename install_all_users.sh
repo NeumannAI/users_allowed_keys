8313105e9b2986728e7b22cac5ecea6ffa530719
Anton <dmvevents@gmail.com>
2021-11-17 17:12:28 +0300

    New Updates

--- install_all_users.sh
+++ install_all_users.sh
@@ -6,28 +6,22 @@ usermod -aG docker $USER
 done
 
 # Always sudo users
-usermod -aG sudo sauhaarda
-usermod -aG sudo sascha
-usermod -aG sudo karlzipser
-usermod -aG sudo yubeic
+usermod -aG sudo anton
+usermod -aG sudo hb
 
 host=`hostname`
-if [ $host == 'bdd3' ]; then # bdd3 sudo users
-    usermod -aG sudo bala
-fi
-if [ $host == 'bdd4' ]; then # bdd4 sudo users
-    usermod -aG sudo aga
-fi
-if [ $host == 'bdd5' ]; then # bdd4 sudo users
-    usermod -aG sudo ehou
-fi
-if [ $host == 'bdd6' ]; then # bdd4 sudo users
-    usermod -aG sudo danqing0703
-    usermod -aG sudo yexia
-fi
+#if [ $host == 'bdd3' ]; then # bdd3 sudo users
+#    usermod -aG sudo bala
+#fi
+#if [ $host == 'bdd4' ]; then # bdd4 sudo users
+#    usermod -aG sudo aga
+#fi
+#if [ $host == 'bdd5' ]; then # bdd4 sudo users
+#    usermod -aG sudo ehou
+#fi
 
 sudo add-apt-repository -y ppa:git-core/ppa
-sudo add-apt-repository -y ppa:neovim-ppa/unstable
-sudo add-apt-repository -y ppa:jonathonf/python-3.6        
+#sudo add-apt-repository -y ppa:neovim-ppa/unstable
+#sudo add-apt-repository -y ppa:jonathonf/python-3.6        
 sudo apt-get -y update
-sudo apt-get -y install neovim tmux zsh highlight python3.6  python3.6-dev python3.6-venv xsel git
+#sudo apt-get -y install neovim tmux zsh highlight python3.6  python3.6-dev python3.6-venv xsel git
